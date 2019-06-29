ed371a3290f18eda594ab30c813eb96b797b4fb1
Rudolf Großmann <rg58@gmx.de>
2019-06-29 16:51:11 +0200

    adjust requirejs.config for jquery

--- js/fa.js
+++ js/fa.js
@@ -1,4 +1,5 @@
 console.log('here is fa.js');
+/*
 requirejs.config({
     //By default load any module IDs from js/lib
     baseUrl: 'js/lib',
@@ -7,10 +8,34 @@ requirejs.config({
     //config is relative to the baseUrl, and
     //never includes a ".js" extension since
     //the paths config could be for a directory.
+    //
     paths: {
         app: '../app'
     }
 });
+*/
+
+// https://requirejs.org/docs/api.html#pathsfallbacks
+requirejs.config({
+    //To get timely, correct error triggers in IE, force a define/shim exports check.
+    enforceDefine: true,
+    paths: {
+        jquery: [
+                /*
+                <script
+                src="https://code.jquery.com/jquery-3.4.1.min.js"
+                integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+                crossorigin="anonymous"></script>
+             'http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min',
+               */
+            'https://code.jquery.com/jquery-3.4.1.min.js',
+
+            //If the CDN location fails, load from this location
+            'lib/jquery-3.4.1.min.js'
+        ]
+    }
+});
+
 
 // Start the main app logic.
 requirejs(['jquery', 'canvas', 'app/sub'],
