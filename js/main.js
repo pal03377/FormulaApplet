4aac1cec33554311ff01f36bf1d206f8a3aa61e6
Rudolf Großmann <rg58@gmx.de>
2019-07-08 22:52:22 +0200

    lib mathquill

--- js/main.js
+++ js/main.js
@@ -13,13 +13,11 @@ function loader_log( logger, message, state){
 }
 
 out_log = document.getElementById( 'load_logger');
-// loader_log( out_log, 'bli', 'dummy');
-// loader_log( out_log, 'bla', 'error');
-// loader_log( out_log, 'blu', 'ok');
+loader_log(out_log, 'main.js loaded.', 'ok');
 
 function load_lib_or_fallback(load_url, fallback){
   loadjs([load_url], {returnPromise: true})
-  .then(function() { loader_log( out_log, load_url + 'loaded', 'ok' ) })
+  .then(function() { loader_log( out_log, load_url + ' loaded', 'ok' ) })
   .catch(function(pathsNotFound) { 
     loader_log( out_log, 'Error loading ' + load_url, 'error' );
       loadjs([fallback], {returnPromise: true})
@@ -28,4 +26,7 @@ function load_lib_or_fallback(load_url, fallback){
     });
 }
 
-load_lib_or_fallback('https://code.jquery.com/jqueryx-3.4.1.min.js', '/js/lib/jquery-3.4.1.min.js')
+load_lib_or_fallback('https://code.jquery.com/jquery-3.4.1.min.js', '/js/lib/jquery-3.4.1.min.js')
+// MathQuill depends on jQuery 1.5.2+
+load_lib_or_fallback('https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.js', '/js/lib/mathquill.js')
+//load_lib_or_fallback('https://code.jquery.com/jquery-3.4.1.min.js', '/js/lib/jquery-3.4.1.min.js')
