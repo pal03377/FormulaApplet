a68f1a765c4515c15aef3e8246c125d219a779a9
Rudolf Großmann <rg58@gmx.de>
2019-07-08 08:33:19 +0200

    load jQuery (in main.js)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 console.log( 'Here is main.js' );
+loadjs(['https://code.jquery.com/jqueryx-3.4.1.min.js'], {returnPromise: true})
+  .then(function() { console.log( 'Here is jQuery' ) })
+  .catch(function(pathsNotFound) { 
+      console.log( 'Could not load jQuery' );
+      loadjs(['/js/lib/jquery-3.4.1.min.js'], {returnPromise: true})
+        .then(function() { console.log( 'Here is jQuery (local)' ) })
+        .catch(function(pathsNotFound) { console.log( 'Could not load jQuery (local)' ) });
+    });
