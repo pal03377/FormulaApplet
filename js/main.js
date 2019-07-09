2fe97a4984142a8a192e96447c457a5d29248e0f
Rudolf Großmann <rg58@gmx.de>
2019-07-09 23:09:35 +0200

    Algebrite into /js/lib

--- js/main.js
+++ js/main.js
@@ -18,20 +18,25 @@ function loader_log( logger, message, state){
 out_log = document.getElementById( 'load_logger');
 loader_log(out_log, 'main.js loaded.', 'ok');
 
-function load_lib_or_fallback(load_url, fallback){
+function load_lib_or_fallback(load_url, fallback, after_fn){
   loadjs([load_url], {returnPromise: true})
-  .then(function() { loader_log( out_log, load_url + ' loaded', 'ok' ) })
+  .then(function() { loader_log( out_log, load_url + ' loaded', 'ok' ); after_fn() })
   .catch(function(pathsNotFound) { 
     loader_log( out_log, 'Error loading ' + load_url, 'error' );
       loadjs([fallback], {returnPromise: true})
-        .then(function() { loader_log( out_log, fallback + ' (local) loaded', 'ok' ) })
+        .then(function() { loader_log( out_log, fallback + ' (local) loaded', 'ok' ); after_fn() })
         .catch(function(pathsNotFound) { loader_log( out_log, 'Error loading ' + fallback + ' (local)', 'error' ) });
     });
 }
 
-load_lib_or_fallback('https://code.jquery.com/jquery-3.4.1.min.js', '/js/lib/jquery-3.4.1.min.js')  // MathQuill needs jQuery 1.5.2+
+load_lib_or_fallback('https://code.jquery.com/jquery-3.4.1.min.js', '/js/lib/jquery-3.4.1.min.js', load_2)  // MathQuill needs jQuery 1.5.2+
 // mathquill: local lib from https://github.com/mathquill/mathquill/releases/download/v0.10.1/mathquill-0.10.1.zip (NOT source, lacks of mathquill.js)
-load_lib_or_fallback('https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.js', '/js/lib/mathquill-0.10.1/mathquill.js')
-load_lib_or_fallback('https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.css', '/js/lib/mathquill-0.10.1/mathquill.css')
 
+function load_2(){
+  load_lib_or_fallback('https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.css', '/js/lib/mathquill-0.10.1/mathquill.css', function(){});
+  load_lib_or_fallback('https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.js', '/js/lib/mathquill-0.10.1/mathquill.js', load_3);
+}
 
+function load_3(){
+  loader_log(out_log, 'ready.', '');
+}
