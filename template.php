1f151252f30d9484ede18141adab51d7a4d508c8
Rudolf Großmann <rg58@gmx.de>
2019-07-19 12:42:27 +0200

    create template.php

---
+++ template.php
@@ -0,0 +1,15 @@
+<?php $title='Test Page - Template (gf09) ' ?>
+<?php include_once( 'header.php' ); ?>
+  
+  <!-- link rel="stylesheet" href="./Algebrite_files/styles.css" -->
+  <!--[if lt IE 9]>
+      <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+      <![endif]-->
+</head>
+
+<body>
+<h1><?php echo $title; ?></h1>
+
+<h2>Header </h2>
+
+<?php include_once( 'footer.php' ); ?>
