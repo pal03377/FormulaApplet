24fbaf64ed73c5fcd4a077397fdf13a99c912baf
Rudolf Großmann <rg58@gmx.de>
2019-08-12 18:33:21 +0200

    Install requirejs at a better place

--- header.php
+++ header.php
@@ -2,8 +2,8 @@
 <html lang="de">
 
 <head>
-  <script async src="/js/loadjs.js" type="text/javascript"></script>
-  <script async src="/js/main.js" type="text/javascript"></script>
+  <script data-main="/js/app.js" src="/js/requirejs/require.js"></script>
+  <!-- script async src="/js/main.js" type="text/javascript"></script -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php echo $title; ?></title>
