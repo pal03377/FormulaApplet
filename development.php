740b53cad453c13e5b8902f0feb2b1065838326a
Rudolf Großmann <rg58@gmx.de>
2020-09-27 16:16:07 +0200

    new link structure using page development.php

---
+++ development.php
@@ -0,0 +1,20 @@
+<?php 
+  $title='Development';
+  $liblist = "['end' ]";
+?>
+<?php include_once( 'header.php' ); ?>
+<link href="css/table.css" rel="stylesheet">
+</head>
+
+<body>
+    <h1><?php echo $title; ?></h1>
+    <p><a href='tex_parser.php'>TEX Parser</a></p>
+    <p><a href='check_if_equal.php'>Check if equal</a></p>
+    <p><a href='editable_in_static_MQ.php'>editable_in_static_MQ.php</a></p>
+    <p><a href='decode_encode.php'>Decode - Encode</a></p>
+    <p><a href='mathquill2tex2parser.php'>MathQuill <-> TEX -> Parser</a></p>
+    <p><a href='mathquill2tex2tree.php'>MathQuill <-> TEX -> Tree</a> and tree2TEX check</p>
+    <p><a href='check_unit_decomposition.php'>Check Unit Decomposition</a></p>
+    <p><a href='mathquill2tex2parser_no_feedback.php'>MathQuill -> TEX (without feedback) -> Parser </a></p>
+    <p><a href='other.php'>Other solutions</a> KAS, Algebrite, MathQuill</p>
+  <?php include_once( 'footer.php' ); ?>
--- index.php
+++ index.php
@@ -8,18 +8,10 @@
 
 <body>
     <h1><?php echo $title; ?></h1>
-    <p><a href='tex_parser.php'>TEX Parser</a></p>
-    <p><a href='check_if_equal.php'>Check if equal</a></p>
-    <p><a href='editable_in_static_MQ.php'>editable_in_static_MQ.php</a></p>
-    <p><a href='decode_encode.php'>Decode - Encode</a></p>
-    <p><a href='mathquill2tex2parser.php'>MathQuill <-> TEX -> Parser</a></p>
-    <p><a href='mathquill2tex2tree.php'>MathQuill <-> TEX -> Tree</a> and tree2TEX check</p>
-    <p><a href='check_unit_decomposition.php'>Check Unit Decomposition</a></p>
-    <p><a href='mathquill2tex2parser_no_feedback.php'>MathQuill -> TEX (without feedback) -> Parser </a></p>
-    <p><a href='decode_encode.php'>Test JSZip (see console)</a></p>
     <p><a href='sample_task.php'>Sample task</a> for later use in MediaWiki</p>
+    <p><a href='development.php'>Development</a></p>
+    
     <!-- <p><a href='background_pic.php'>background pictures </a> for 'wrong' and 'ok'</p> -->
-    <p><a href='other.php'>Other solutions</a> KAS, Algebrite, MathQuill</p>
     
   <hr>
   <p><a href='https://github.com/gro58/gf09'>gf09 at GitHub</a></p>
